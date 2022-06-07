fde8aa2e494188591e5032555671f8916f6bbcd1
BA <laugholouder@gmail.com>
2022-06-08 03:47:46 +0800

    ✨ Delay/Repeat by Unity time

--- UpdateHub.cs
+++ UpdateHub.cs
@@ -18,11 +18,13 @@ namespace BAStudio.UpdateHub
             OnFixedUpdate?.Invoke();
         }
 
-        public UpdateHandle NewUpdate (Action action) => new UpdateHandle(this, action);
+        public UpdateHandle      NewUpdate (Action action) => new UpdateHandle(this, action);
         public FixedUpdateHandle NewFixedUpdate (Action action) => new FixedUpdateHandle(this, action);
-        public DelayByFrames NewDelayByFrames (int frames, Action action) => new DelayByFrames(this, frames, action);
-        public DelayByFixedTime NewDelayByFixedTime (float delay, Action action) => new DelayByFixedTime(this, delay, action);
-        public RepeatByFrames NewRepeatByFrames (int frames, Action action) => new RepeatByFrames(this, frames, action);
+        public DelayByFrames     NewDelayByFrames (int frames, Action action) => new DelayByFrames(this, frames, action);
+        public DelayByTime       NewDelayByTime (float delay, Action action) => new DelayByTime(this, delay, action);
+        public DelayByFixedTime  NewDelayByFixedTime (float delay, Action action) => new DelayByFixedTime(this, delay, action);
+        public RepeatByFrames    NewRepeatByFrames (int frames, Action action) => new RepeatByFrames(this, frames, action);
+        public RepeatByTime      NewRepeatByTime (float interval, Action action) => new RepeatByTime(this, interval, action);
         public RepeatByFixedTime NewRepeatByFixedTime (float interval, Action action) => new RepeatByFixedTime(this, interval, action);
 
         public class UpdateHandle : IDisposable
@@ -79,6 +81,36 @@ namespace BAStudio.UpdateHub
             void Next ()
             {
                 if (countdown-- <= 0)
+                {
+                    Action.Invoke();
+                    Dispose();
+                }
+            }
+
+            public void Dispose()
+            {
+                Hub = null;
+                Hub.OnUpdate -= Action;
+            }
+        }
+
+        public class DelayByTime : IDisposable
+        {
+            public UpdateHub Hub { get; protected set; }
+            public Action Action { get; protected set; }
+            float start, end;
+            public DelayByTime (UpdateHub hub, float delay, Action action)
+            {
+                Hub = hub;
+                Action = action;
+                hub.OnUpdate += Next;
+                this.start = Time.time;
+                this.end = Time.time + delay;
+            }
+
+            void Next ()
+            {
+                if (Time.time >= end)
                 {
                     Action.Invoke();
                     Dispose();
@@ -150,7 +182,37 @@ namespace BAStudio.UpdateHub
                 Hub.OnUpdate -= Action;
             }
         }
-        
+
+        public class RepeatByTime : IDisposable
+        {
+            public UpdateHub Hub { get; protected set; }
+            public Action Action { get; protected set; }
+            float last, interval;
+            public RepeatByTime (UpdateHub hub, float interval, Action action)
+            {
+                Hub = hub;
+                Action = action;
+                hub.OnUpdate += Next;
+                this.interval = interval;
+                this.last = Time.time;
+            }
+
+            void Next ()
+            {
+                if (last + interval >= Time.time)
+                {
+                    last = Time.time;
+                    Action.Invoke();
+                }
+            }
+
+            public void Dispose()
+            {
+                Hub = null;
+                Hub.OnUpdate -= Action;
+            }
+        }
+
         public class RepeatByFixedTime : IDisposable
         {
             public UpdateHub Hub { get; protected set; }
